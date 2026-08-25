c5b41ed6501ae913b4bbb237118a5ee7ea9d2ec0
David Belmez Vicente <sanmibuh@gmail.com>
2026-08-25 08:30:16 +0200

    [improvement/GH-66-tdd-archunit-classes-impor]: [Refactor] Update HexagonalArchitectureTest to use ArchUnit annotations for improved readability and structure (#88)

--- src/test/java/org/sanmibuh/tedee/automation/HexagonalArchitectureTest.java
+++ src/test/java/org/sanmibuh/tedee/automation/HexagonalArchitectureTest.java
@@ -2,31 +2,23 @@ package org.sanmibuh.tedee.automation;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
-import org.junit.jupiter.api.Test;
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
 
+@AnalyzeClasses(
+  packages = "org.sanmibuh")
 class HexagonalArchitectureTest {
 
-  private static final String BASE_PACKAGE = "org.sanmibuh";
+  @ArchTest
+  static final ArchRule should_forbidSpringDependencies_whenInDomainLayer = noClasses()
+    .that().resideInAPackage("..domain..")
+    .should().dependOnClassesThat()
+    .resideInAPackage("org.springframework..");
 
-  private final JavaClasses classes = new ClassFileImporter().importPackages(BASE_PACKAGE);
-
-  @Test
-  void should_forbidSpringDependencies_whenInDomainLayer() {
-    final var rule = noClasses()
-      .that().resideInAPackage("..domain..")
-      .should().dependOnClassesThat()
-      .resideInAPackage("org.springframework..");
-    rule.check(classes);
-  }
-
-  @Test
-  void should_forbidInfrastructureDependencies_whenInDomainLayer() {
-    final var rule = noClasses()
-      .that().resideInAPackage("..domain..")
-      .should().dependOnClassesThat()
-      .resideInAPackage("..infrastructure..");
-    rule.check(classes);
-  }
+  @ArchTest
+  static final ArchRule should_forbidInfrastructureDependencies_whenInDomainLayer = noClasses()
+    .that().resideInAPackage("..domain..")
+    .should().dependOnClassesThat()
+    .resideInAPackage("..infrastructure..");
 }
