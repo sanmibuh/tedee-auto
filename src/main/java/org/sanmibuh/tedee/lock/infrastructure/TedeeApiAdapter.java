fd24b2e6546ebba0af9a94e8bd752f372604803a
David Belmez <davidbvic@inditex.com>
2026-08-28 17:49:22 +0200

    feat: translate bridge errors to outcome-based exceptions

--- src/main/java/org/sanmibuh/tedee/lock/infrastructure/TedeeApiAdapter.java
+++ src/main/java/org/sanmibuh/tedee/lock/infrastructure/TedeeApiAdapter.java
@@ -2,13 +2,11 @@ package org.sanmibuh.tedee.lock.infrastructure;
 
 import com.tedee.bridge.client.api.LockApi;
 import lombok.RequiredArgsConstructor;
-import org.sanmibuh.tedee.lock.domain.InvalidApiTokenException;
-import org.sanmibuh.tedee.lock.domain.LockBleErrorException;
-import org.sanmibuh.tedee.lock.domain.LockDisconnectedException;
+import org.sanmibuh.tedee.lock.domain.InvalidLockRequestException;
 import org.sanmibuh.tedee.lock.domain.LockId;
-import org.sanmibuh.tedee.lock.domain.LockNotFoundException;
+import org.sanmibuh.tedee.lock.domain.LockOperationFailedException;
 import org.sanmibuh.tedee.lock.domain.LockPort;
-import org.sanmibuh.tedee.lock.domain.UnexpectedBridgeErrorException;
+import org.sanmibuh.tedee.lock.domain.LockTemporarilyUnavailableException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
@@ -31,11 +29,10 @@ public class TedeeApiAdapter implements LockPort {
   private RuntimeException toDomainException(
       final HttpClientErrorException exception, final LockId lockId) {
     return switch (HttpStatus.resolve(exception.getStatusCode().value())) {
-      case UNAUTHORIZED -> new InvalidApiTokenException();
-      case NOT_FOUND -> new LockNotFoundException(lockId.deviceId());
-      case METHOD_NOT_ALLOWED -> new LockDisconnectedException(lockId.deviceId());
-      case NOT_ACCEPTABLE -> new LockBleErrorException(lockId.deviceId());
-      case null, default -> new UnexpectedBridgeErrorException(lockId.deviceId(), exception);
+      case NOT_FOUND -> new InvalidLockRequestException(lockId.deviceId(), exception);
+      case METHOD_NOT_ALLOWED, NOT_ACCEPTABLE ->
+          new LockTemporarilyUnavailableException(lockId.deviceId(), exception);
+      case null, default -> new LockOperationFailedException(lockId.deviceId(), exception);
     };
   }
 }
