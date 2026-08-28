030f4d334ef6ba8872ce05a4873b517c14982b86
David Belmez <davidbvic@inditex.com>
2026-08-28 17:17:13 +0200

    feat: translate bridge 405 to LockDisconnectedException when locking

--- src/main/java/org/sanmibuh/tedee/lock/infrastructure/TedeeApiAdapter.java
+++ src/main/java/org/sanmibuh/tedee/lock/infrastructure/TedeeApiAdapter.java
@@ -3,6 +3,7 @@ package org.sanmibuh.tedee.lock.infrastructure;
 import com.tedee.bridge.client.api.LockApi;
 import lombok.RequiredArgsConstructor;
 import org.sanmibuh.tedee.lock.domain.InvalidApiTokenException;
+import org.sanmibuh.tedee.lock.domain.LockDisconnectedException;
 import org.sanmibuh.tedee.lock.domain.LockId;
 import org.sanmibuh.tedee.lock.domain.LockNotFoundException;
 import org.sanmibuh.tedee.lock.domain.LockPort;
@@ -31,6 +32,7 @@ public class TedeeApiAdapter implements LockPort {
     return switch (HttpStatus.resolve(exception.getStatusCode().value())) {
       case UNAUTHORIZED -> new InvalidApiTokenException();
       case NOT_FOUND -> new LockNotFoundException(lockId.deviceId());
+      case METHOD_NOT_ALLOWED -> new LockDisconnectedException(lockId.deviceId());
       case null, default -> new UnexpectedBridgeErrorException(lockId.deviceId(), exception);
     };
   }
