9d0c497e6f01c58442bf990a11f07199a8a9c909
David Belmez <davidbvic@inditex.com>
2026-08-28 16:47:33 +0200

    feat: translate bridge 401 to InvalidApiTokenException when locking

--- src/main/java/org/sanmibuh/tedee/lock/infrastructure/TedeeApiAdapter.java
+++ src/main/java/org/sanmibuh/tedee/lock/infrastructure/TedeeApiAdapter.java
@@ -2,9 +2,12 @@ package org.sanmibuh.tedee.lock.infrastructure;
 
 import com.tedee.bridge.client.api.LockApi;
 import lombok.RequiredArgsConstructor;
+import org.sanmibuh.tedee.lock.domain.InvalidApiTokenException;
 import org.sanmibuh.tedee.lock.domain.LockId;
 import org.sanmibuh.tedee.lock.domain.LockPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Component
 @RequiredArgsConstructor
@@ -14,6 +17,13 @@ public class TedeeApiAdapter implements LockPort {
 
   @Override
   public void lock(final LockId lockId) {
-    lockApi.postLock(lockId.deviceId());
+    try {
+      lockApi.postLock(lockId.deviceId());
+    } catch (final HttpClientErrorException exception) {
+      if (exception.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+        throw new InvalidApiTokenException();
+      }
+      throw exception;
+    }
   }
 }
