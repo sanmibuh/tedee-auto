27277b410eb9addf637176eaabb556236161a8e3
David Belmez <davidbvic@inditex.com>
2026-08-29 00:16:02 +0200

    feat: fail with AggregateNotFoundException when the lock is absent

--- src/main/java/org/sanmibuh/tedee/lock/application/CloseLockHandler.java
+++ src/main/java/org/sanmibuh/tedee/lock/application/CloseLockHandler.java
@@ -2,6 +2,8 @@ package org.sanmibuh.tedee.lock.application;
 
 import lombok.RequiredArgsConstructor;
 import org.sanmibuh.cqrs.domain.CommandHandler;
+import org.sanmibuh.ddd.domain.AggregateNotFoundException;
+import org.sanmibuh.tedee.lock.domain.Lock;
 import org.sanmibuh.tedee.lock.domain.LockId;
 import org.sanmibuh.tedee.lock.domain.LockPort;
 
@@ -12,7 +14,11 @@ public class CloseLockHandler implements CommandHandler<CloseLockCommand> {
 
   @Override
   public void handle(final CloseLockCommand command) {
-    final var lock = lockPort.findById(new LockId(command.deviceId())).orElseThrow();
+    final var lockId = new LockId(command.deviceId());
+    final var lock =
+        lockPort
+            .findById(lockId)
+            .orElseThrow(() -> new AggregateNotFoundException(Lock.class, lockId));
     lock.lock();
     lockPort.save(lock);
   }
