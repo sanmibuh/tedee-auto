2513cd37d0cc11ce5785fbed4e982ad5a27c1f9a
David Belmez <davidbvic@inditex.com>
2026-08-28 16:46:00 +0200

    test: expect InvalidApiTokenException when bridge responds 401

--- src/test/java/org/sanmibuh/tedee/lock/infrastructure/TedeeApiAdapterTest.java
+++ src/test/java/org/sanmibuh/tedee/lock/infrastructure/TedeeApiAdapterTest.java
@@ -1,16 +1,20 @@
 package org.sanmibuh.tedee.lock.infrastructure;
 
+import static org.assertj.core.api.BDDAssertions.thenThrownBy;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withNoContent;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 
 import org.junit.jupiter.api.Test;
+import org.sanmibuh.tedee.lock.domain.InvalidApiTokenException;
 import org.sanmibuh.tedee.lock.domain.LockId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.restclient.test.autoconfigure.RestClientTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.client.MockRestServiceServer;
 
@@ -49,5 +53,15 @@ class TedeeApiAdapterTest {
     sut.lock(new LockId(42));
 
     server.verify();
+  }
+
+  @Test
+  void should_throwInvalidApiToken_whenBridgeRespondsUnauthorized() {
+    server
+        .expect(requestTo("http://localhost/v1.0/lock/42/lock"))
+        .andExpect(method(HttpMethod.POST))
+        .andRespond(withStatus(HttpStatus.UNAUTHORIZED));
+
+    thenThrownBy(() -> sut.lock(new LockId(42))).isInstanceOf(InvalidApiTokenException.class);
   }
 }
