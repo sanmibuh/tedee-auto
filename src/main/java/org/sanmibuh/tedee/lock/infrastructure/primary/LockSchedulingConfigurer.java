ee5780b5e33d13810f21263178dbd4e75b07312f
David Belmez <davidbvic@inditex.com>
2026-08-29 18:12:59 +0200

    test: LockSchedulingConfigurer registers a cron task per schedule

---
+++ src/main/java/org/sanmibuh/tedee/lock/infrastructure/primary/LockSchedulingConfigurer.java
@@ -0,0 +1,17 @@
+package org.sanmibuh.tedee.lock.infrastructure.primary;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@RequiredArgsConstructor
+public class LockSchedulingConfigurer implements SchedulingConfigurer {
+
+  private final LockSchedulerProperties properties;
+  private final LockScheduler scheduler;
+
+  @Override
+  public void configureTasks(final ScheduledTaskRegistrar registrar) {
+    throw new UnsupportedOperationException();
+  }
+}
---
+++ src/test/java/org/sanmibuh/tedee/lock/infrastructure/primary/LockSchedulingConfigurerTest.java
@@ -0,0 +1,34 @@
+package org.sanmibuh.tedee.lock.infrastructure.primary;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.scheduling.config.CronTask;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@ExtendWith(MockitoExtension.class)
+class LockSchedulingConfigurerTest {
+
+  private static final int DEVICE_ID = 12345;
+  private static final String CRON = "0 0 0,22 * * *";
+
+  @Mock LockScheduler scheduler;
+
+  @Test
+  void should_registerCronTaskWithConfiguredExpression_whenConfiguringTasks() {
+    final var properties = new LockSchedulerProperties(Map.of(DEVICE_ID, CRON));
+    final var sut = new LockSchedulingConfigurer(properties, scheduler);
+    final var registrar = new ScheduledTaskRegistrar();
+
+    sut.configureTasks(registrar);
+
+    then(registrar.getCronTaskList())
+        .singleElement()
+        .extracting(CronTask::getExpression)
+        .isEqualTo(CRON);
+  }
+}
