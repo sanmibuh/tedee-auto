130cd6f86e976a65e2a534d5ebacf6efd16e0eef
David Belmez <davidbvic@inditex.com>
2026-08-31 14:57:16 +0200

    refactor: streamline scanner setup in TedeeBridgeRuntimeHints and delegate main method call in tests

--- src/main/java/org/sanmibuh/tedee/lock/infrastructure/secondary/TedeeBridgeRuntimeHints.java
+++ src/main/java/org/sanmibuh/tedee/lock/infrastructure/secondary/TedeeBridgeRuntimeHints.java
@@ -22,23 +22,20 @@ class TedeeBridgeRuntimeHints implements RuntimeHintsRegistrar {
 
   @Override
   public void registerHints(final RuntimeHints hints, final @Nullable ClassLoader classLoader) {
-    configureScannerPITEquivalent(classLoader).findCandidateComponents(MODEL_PACKAGE).stream()
+    var scanner =
+        setResourceLoaderPITEquivalent(
+            new ClassPathScanningCandidateComponentProvider(false), classLoader);
+    scanner.addIncludeFilter(new AssignableTypeFilter(Object.class));
+    scanner.findCandidateComponents(MODEL_PACKAGE).stream()
         .flatMap(bd -> loadWithDeclaredClasses(bd, classLoader))
         .forEach(type -> hints.reflection().registerType(type, JACKSON_CATEGORIES));
   }
 
-  private ClassPathScanningCandidateComponentProvider configureScannerPITEquivalent(
-      final @Nullable ClassLoader classLoader) {
-    var scanner = new ClassPathScanningCandidateComponentProvider(false);
-    scanner.setResourceLoader(new DefaultResourceLoader(classLoader));
-    scanner.addIncludeFilter(new AssignableTypeFilter(Object.class));
-    return scanner;
-  }
-
-  private void configureScannerPITEquivalent(
+  private ClassPathScanningCandidateComponentProvider setResourceLoaderPITEquivalent(
       final ClassPathScanningCandidateComponentProvider scanner,
       final @Nullable ClassLoader classLoader) {
     scanner.setResourceLoader(new DefaultResourceLoader(classLoader));
+    return scanner;
   }
 
   private Stream<Class<?>> loadWithDeclaredClasses(
--- src/test/java/org/sanmibuh/tedee/TedeeAutomationApplicationMainTest.java
+++ src/test/java/org/sanmibuh/tedee/TedeeAutomationApplicationMainTest.java
@@ -2,16 +2,19 @@ package org.sanmibuh.tedee;
 
 import static org.mockito.Mockito.mockStatic;
 
+import java.util.function.Consumer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 
 class TedeeAutomationApplicationMainTest {
 
+  private final Consumer<String[]> sut = TedeeAutomationApplication::main;
+
   @Test
   void should_delegateToSpringApplication_whenMainIsCalled() {
     final var args = new String[] {};
     try (var mocked = mockStatic(SpringApplication.class)) {
-      TedeeAutomationApplication.main(args);
+      sut.accept(args);
 
       mocked.verify(() -> SpringApplication.run(TedeeAutomationApplication.class, args));
     }
