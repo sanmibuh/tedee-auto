b5a59a06032bfc59463a0cfea19e933c9c65d8a0
David Belmez <davidbvic@inditex.com>
2026-09-01 23:28:23 +0200

    test: cover idempotent registration in DomainEventHandlerRegistrar

--- src/test/java/org/sanmibuh/ddd/infrastructure/DomainEventHandlerRegistrarTest.java
+++ src/test/java/org/sanmibuh/ddd/infrastructure/DomainEventHandlerRegistrarTest.java
@@ -2,8 +2,10 @@ package org.sanmibuh.ddd.infrastructure;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
+import com.example.StubEventHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 
 class DomainEventHandlerRegistrarTest {
@@ -27,5 +29,18 @@ class DomainEventHandlerRegistrarTest {
     registrar.postProcessBeanDefinitionRegistry(beanFactory);
 
     then(beanFactory.containsBeanDefinition("stubEventHandler")).isTrue();
+  }
+
+  @Test
+  void should_notRegisterHandlerBean_whenAlreadyRegistered() {
+    final var beanFactory = new DefaultListableBeanFactory();
+    AutoConfigurationPackages.register(beanFactory, "com.example");
+    beanFactory.registerBeanDefinition(
+        "myEventHandler", new RootBeanDefinition(StubEventHandler.class));
+
+    final var registrar = new DomainEventHandlerRegistrar(beanFactory);
+    registrar.postProcessBeanDefinitionRegistry(beanFactory);
+
+    then(beanFactory.getBeansOfType(StubEventHandler.class)).hasSize(1);
   }
 }
