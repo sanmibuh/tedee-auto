2f7676b54aa1b3e291de9b7d7bf5d611d5e2e007
David Belmez <davidbvic@inditex.com>
2026-09-01 23:36:20 +0200

    test: AggregateRoot equality is by identity, ignoring domain events

--- src/test/java/org/sanmibuh/ddd/domain/AggregateRootTest.java
+++ src/test/java/org/sanmibuh/ddd/domain/AggregateRootTest.java
@@ -3,9 +3,16 @@ package org.sanmibuh.ddd.domain;
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.assertj.core.api.BDDAssertions.thenThrownBy;
 
+import org.assertj.core.api.BDDSoftAssertions;
+import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(SoftAssertionsExtension.class)
 class AggregateRootTest {
+
+  @InjectSoftAssertions private BDDSoftAssertions softly;
 
   private final TestAggregate sut = new TestAggregate(new TestId(42));
 
@@ -31,6 +38,25 @@ class AggregateRootTest {
         .isInstanceOf(UnsupportedOperationException.class);
   }
 
+  @Test
+  void should_beEqualIgnoringEvents_whenSameTypeAndSameId() {
+    final var other = new TestAggregate(new TestId(42));
+    other.emit(new TestEvent());
+
+    softly.then(sut).isEqualTo(other);
+    softly.then(sut).hasSameHashCodeAs(other);
+  }
+
+  @Test
+  void should_notBeEqual_whenSameTypeButDifferentId() {
+    then(sut).isNotEqualTo(new TestAggregate(new TestId(7)));
+  }
+
+  @Test
+  void should_notBeEqual_whenDifferentTypeButSameId() {
+    then(sut).isNotEqualTo(new OtherAggregate(new TestId(42)));
+  }
+
   private static final class TestAggregate extends AggregateRoot<TestId> {
     TestAggregate(final TestId id) {
       super(id);
@@ -38,6 +64,12 @@ class AggregateRootTest {
 
     void emit(final DomainEvent event) {
       recordEvent(event);
+    }
+  }
+
+  private static final class OtherAggregate extends AggregateRoot<TestId> {
+    OtherAggregate(final TestId id) {
+      super(id);
     }
   }
 
