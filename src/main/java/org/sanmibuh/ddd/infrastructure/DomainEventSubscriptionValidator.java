ef5b67fa85569c76940c958be9f2934bb2f319fa
David Belmez <davidbvic@inditex.com>
2026-09-03 08:42:31 +0200

    feat: skip subscriber check for events annotated with @NoSubscribersRequired

--- src/main/java/org/sanmibuh/ddd/infrastructure/DomainEventSubscriptionValidator.java
+++ src/main/java/org/sanmibuh/ddd/infrastructure/DomainEventSubscriptionValidator.java
@@ -2,13 +2,15 @@ package org.sanmibuh.ddd.infrastructure;
 
 import java.util.Collection;
 import java.util.Set;
+import org.sanmibuh.ddd.domain.NoSubscribersRequired;
 
 final class DomainEventSubscriptionValidator {
 
   void validate(
       final Collection<Class<?>> publishableEvents, final Set<Class<?>> handledEventTypes) {
     for (final var eventType : publishableEvents) {
-      if (!handledEventTypes.contains(eventType)) {
+      if (!handledEventTypes.contains(eventType)
+          && !eventType.isAnnotationPresent(NoSubscribersRequired.class)) {
         throw new MissingEventHandlerException(eventType);
       }
     }
