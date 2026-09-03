a70e8f999a8b2cbe1b4bb4a5caf7e5949e38cde7
David Belmez <davidbvic@inditex.com>
2026-09-03 08:34:41 +0200

    test: allow domain events to opt out of requiring subscribers

--- src/test/java/org/sanmibuh/ddd/infrastructure/DomainEventSubscriptionValidatorTest.java
+++ src/test/java/org/sanmibuh/ddd/infrastructure/DomainEventSubscriptionValidatorTest.java
@@ -1,11 +1,13 @@
 package org.sanmibuh.ddd.infrastructure;
 
+import static org.assertj.core.api.BDDAssertions.thenNoException;
 import static org.assertj.core.api.BDDAssertions.thenThrownBy;
 
 import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.sanmibuh.ddd.domain.DomainEvent;
+import org.sanmibuh.ddd.domain.NoSubscribersRequired;
 
 class DomainEventSubscriptionValidatorTest {
 
@@ -18,5 +20,13 @@ class DomainEventSubscriptionValidatorTest {
         .hasMessageContaining(UnhandledEvent.class.getName());
   }
 
+  @Test
+  void should_notThrowException_whenPublishableEventOptsOutOfSubscribers() {
+    thenNoException().isThrownBy(() -> sut.validate(List.of(OptedOutEvent.class), Set.of()));
+  }
+
   record UnhandledEvent() implements DomainEvent {}
+
+  @NoSubscribersRequired
+  record OptedOutEvent() implements DomainEvent {}
 }
