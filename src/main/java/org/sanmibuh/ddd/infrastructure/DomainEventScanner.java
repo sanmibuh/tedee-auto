e380889b5d6ffe1a6cff468565a729d08d5a37f8
David Belmez <davidbvic@inditex.com>
2026-09-03 08:46:45 +0200

    test: discover domain event types in scanned packages

---
+++ src/main/java/org/sanmibuh/ddd/infrastructure/DomainEventScanner.java
@@ -0,0 +1,11 @@
+package org.sanmibuh.ddd.infrastructure;
+
+import java.util.List;
+import java.util.Set;
+
+final class DomainEventScanner {
+
+  Set<Class<?>> scan(final List<String> packages) {
+    return Set.of();
+  }
+}
---
+++ src/test/java/com/example/StubDomainEvent.java
@@ -0,0 +1,5 @@
+package com.example;
+
+import org.sanmibuh.ddd.domain.DomainEvent;
+
+public record StubDomainEvent() implements DomainEvent {}
