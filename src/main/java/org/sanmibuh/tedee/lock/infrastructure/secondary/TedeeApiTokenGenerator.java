b105d79bbc5afd4e9f693971499a837f3fad1b07
David Belmez <davidbvic@inditex.com>
2026-09-18 17:14:25 +0200

    feat: generate encrypted api_token for Tedee Bridge authentication

--- src/main/java/org/sanmibuh/tedee/lock/infrastructure/secondary/TedeeApiTokenGenerator.java
+++ src/main/java/org/sanmibuh/tedee/lock/infrastructure/secondary/TedeeApiTokenGenerator.java
@@ -1,13 +1,26 @@
 package org.sanmibuh.tedee.lock.infrastructure.secondary;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.HexFormat;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 
 @RequiredArgsConstructor
 final class TedeeApiTokenGenerator {
 
+  private static final String HASH_ALGORITHM = "SHA-256";
+
   private final String token;
 
   String generate(final long timestampMillis) {
-    throw new UnsupportedOperationException();
+    return sha256Hex(token + timestampMillis) + timestampMillis;
+  }
+
+  @SneakyThrows
+  private String sha256Hex(final String value) {
+    final var digest =
+        MessageDigest.getInstance(HASH_ALGORITHM).digest(value.getBytes(StandardCharsets.UTF_8));
+    return HexFormat.of().formatHex(digest);
   }
 }
