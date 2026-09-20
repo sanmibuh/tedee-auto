d4b373b84364190a14c26f577940e8c7086211f7
David Belmez <davidbvic@inditex.com>
2026-09-20 20:49:02 +0200

    test: lock the default UTC timezone binding for the lock scheduler properties

---
+++ src/test/java/org/sanmibuh/tedee/lock/infrastructure/primary/LockSchedulerPropertiesTest.java
@@ -0,0 +1,25 @@
+package org.sanmibuh.tedee.lock.infrastructure.primary;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+class LockSchedulerPropertiesTest {
+
+  private final ApplicationContextRunner runner =
+      new ApplicationContextRunner().withUserConfiguration(EnableLockSchedulerProperties.class);
+
+  @Test
+  void should_bindZoneToUtc_whenZoneIsNotProvided() {
+    runner.run(
+        context -> {
+          final var properties = context.getBean(LockSchedulerProperties.class);
+          then(properties.zone()).isEqualTo("UTC");
+        });
+  }
+
+  @EnableConfigurationProperties(LockSchedulerProperties.class)
+  private static final class EnableLockSchedulerProperties {}
+}
